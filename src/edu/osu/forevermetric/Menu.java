57769b46409aa8ab6ddfeb5cba3fe1a1d67e27f8
zaq831zaq <zaq831zaq@d607edf5-ed88-1384-83c1-3e904f0130a1>
2012-04-27 23:29:26 +0000

    Have a splash that waist 5 seconds then goes to a screen with "Conversion game" button and "Distance Game" button

---
+++ src/edu/osu/forevermetric/Menu.java
@@ -0,0 +1,15 @@
+package edu.osu.forevermetric;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Menu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menu);
+		
+	}
+	
+}
--- src/edu/osu/forevermetric/Splash.java
+++ src/edu/osu/forevermetric/Splash.java
@@ -4,20 +4,33 @@ package edu.osu.forevermetric;
 import edu.osu.forevermetric.R;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
 
 public class Splash extends Activity {
-	
-	Button start;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.splash);
+        
+        // Have the splash screen wait 5 seconds and then move onto the Menu
+		final int sleepTime = 5000;
+		// timer to sleep for 5 seconds
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(sleepTime);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally{
+					Intent openStartingPoint = new Intent("edu.osu.forevermetric.MENU");
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		timer.start();
 	}
 	
 	@Override
