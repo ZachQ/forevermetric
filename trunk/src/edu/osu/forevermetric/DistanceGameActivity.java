48c8d92ec823306f43ea08d81447dd25c1d5f30a
mnm501288@gmail.com <mnm501288@gmail.com@d607edf5-ed88-1384-83c1-3e904f0130a1>
2012-04-30 01:48:19 +0000

    implemented some of the distance game.  Doesnt check for correctness just outputs current distance to destination in meters

---
+++ trunk/src/edu/osu/forevermetric/DistanceGameActivity.java
@@ -0,0 +1,64 @@
+package edu.osu.forevermetric;
+
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class DistanceGameActivity extends Activity implements OnClickListener{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.distance_game);
+		//set question
+		TextView textview = new TextView(this);
+		textview = (TextView) findViewById(R.id.questionTextView);
+		textview.setText("how many meters are you from the Ohio Stadium?" );
+		
+		//create button
+		View guessButton = (Button) findViewById(R.id.distanceGameButton);
+		guessButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.distanceGameButton:
+			//get guess
+			EditText editText = (EditText) findViewById(R.id.usersGuess);
+			String userGuess = editText.getText().toString();
+			//get users location
+			LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+		    Location userLocation = null;
+		    userLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		    //set location of ohio stadium
+			Location questionDestination = new Location("");
+			questionDestination.setLatitude(40.001667);
+			questionDestination.setLongitude(-83.019722);
+			//get distance beween user and location
+			float correctAnswer = userLocation.distanceTo(questionDestination);
+			TextView display = new TextView(this);
+			display = (TextView) findViewById(R.id.answerField);
+			display.setText(Float.toString(correctAnswer));
+			break;
+			
+		}
+		
+	}
+	
+	
+	
+}
--- trunk/src/edu/osu/forevermetric/Menu.java
+++ trunk/src/edu/osu/forevermetric/Menu.java
@@ -1,15 +1,33 @@
 package edu.osu.forevermetric;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class Menu extends Activity {
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Menu extends Activity implements OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu);
 		
+		View distanceButton = (Button) findViewById(R.id.bDistance);
+		distanceButton.setOnClickListener(this);
 	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.bDistance:
+			startActivity(new Intent(this, DistanceStartActivity.class));
+			break;
+		}
+	} 
 	
 }
